c373e066438addac18f8efad4cc183aee3fe554d
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2026-02-17 01:58:17 +0530

    implemented CRUD API and update rules on Schema

--- src/app.js
+++ src/app.js
@@ -3,27 +3,61 @@ const app = express();
 const { adminAuth, userAuth } = require('./middlewares/auth');
 const { connectDB } = require('./config/database');
 const User = require('./models/user');
+const user = require('./models/user');
 
+app.use(express.json());
 app.use('/admin', adminAuth);
 app.get('/admin/allData', (req, res) => {
   res.send('This is the admin Data');
 });
-app.get('/user', userAuth, (req, res) => {
-  res.send('This is the user Data');
-});
 app.post('/signup', async (req, res) => {
-  const user = new User({
-    firstName: 'Rakesh',
-    secondName: 'Singh',
-    email: 'rakesh@123.com',
-  });
+  const data = req.body;
+  const user = new User(data);
   try {
     await user.save();
     res.send('User added in database');
   } catch (error) {
+    console.log(error);
     res.status(400).send('Error saving user');
   }
 });
+app.get('/user', userAuth, async (req, res) => {
+  const emailId = req.body.emailId;
+  try {
+    const users = await user.find({ emailId });
+    res.send(users);
+  } catch (error) {
+    res.status(404).send('User not found');
+  }
+});
+app.get('/feed', userAuth, async (req, res) => {
+  const users = await user.find({});
+  try {
+    res.send(users);
+  } catch (error) {
+    res.status(404).send('Users not found');
+  }
+});
+app.delete('/user', userAuth, async (req, res) => {
+  const emailId = req.body.emailId;
+  try {
+    await user.findOneAndDelete({ emailId });
+    res.send('User deleted succesfully');
+  } catch (error) {
+    res.status(400).send('User delete failed');
+  }
+});
+app.patch('/user', userAuth, async (req, res) => {
+  const userId = req.body.userId;
+  const data = req.body;
+  try {
+    const users = await user.findByIdAndUpdate({ _id: userId }, data);
+    res.send(users);
+  } catch (error) {
+    res.status(400).send('User not updated');
+  }
+});
+
 connectDB()
   .then(() => {
     app.listen(7777, () => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-  firstName: { type: String },
+  firstName: { type: String, required: true, minLength: 4 },
   secondName: { type: String },
-  //   profilePic: { type: String },
-  email: { type: String },
-  password: { type: String },
+  emailId: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true },
   age: { type: Number },
   gender: { type: String },
+  photoUrl: { type: String },
+  about: { type: String, default: 'This is about me' },
+  interest: { type: [String] },
 });
 
 module.exports = mongoose.model('zingUsers', userSchema);
