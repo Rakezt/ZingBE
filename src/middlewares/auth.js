fae526849f6ce32ab00359a7e76b098405c6888e
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2026-02-22 15:28:54 +0530

    updating error response

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      throw new Error('Token is not valid');
+      res.status(401).send(' Token is not valid');
     }
     const decodeMessage = await jwt.verify(token, 'manchester@1992');
     const { _id } = decodeMessage;
