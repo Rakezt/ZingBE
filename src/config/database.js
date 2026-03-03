f8c107bfb6d3b6fad8319cb9ccafd00cde332592
L Rakesh Singh <110081692+Rakezt@users.noreply.github.com>
2026-03-03 13:26:55 +0530

    asdf

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const MONGO_URI =
+  'mongodb://polladmin:polladmin@ac-cejgush-shard-00-00.5xgtyz5.mongodb.net:27017,ac-cejgush-shard-00-01.5xgtyz5.mongodb.net:27017,ac-cejgush-shard-00-02.5xgtyz5.mongodb.net:27017/?ssl=true&replicaSet=atlas-785s4b-shard-0&authSource=admin&retryWrites=true&w=majority';
+
 const connectDB = async () => {
   //   console.log(process.env.MONGO_URI);
-  await mongoose.connect(process.env.MONGO_URI);
+  await mongoose.connect(MONGO_URI);
 };
 
 module.exports = { connectDB };
